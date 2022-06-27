f5bc324620f11d75d5b02b3a7d933cdd9c1da8c0
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-06-27 23:43:22 +0200

    logic for displaying weather information add

--- src/index.js
+++ src/index.js
@@ -21,10 +21,6 @@ function getWeather(lat, lon) {
   return weather;
 }
 
-function displayWeather(json) {
-  console.log(json);
-}
-
 function getGeoLocation(cityName = 'Brussels') {
   const limit = 1;
   const location = fetch(
@@ -36,14 +32,88 @@ function getGeoLocation(cityName = 'Brussels') {
   return location;
 }
 
-// getGeoLocation('london')
-//   .then((response) => {
-//     const { lat } = response[0];
-//     const { lon } = response[0];
-//     getWeather(lat, lon).then(displayWeather);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
 document.body.append(header.getHeader('weather at'), main, footer);
+
+const LocationForm = (() => {
+  const form = document.createElement('form');
+  const input = document.createElement('input');
+  const btnSubmit = document.createElement('button');
+  btnSubmit.textContent = 'search';
+
+  form.append(input, btnSubmit);
+  return form;
+})();
+
+// temp: in kelvin
+const WeatherAtCard = (desc = '', temp = 0) => {
+  const kelvin = temp;
+  const celcius = temp - 273.15;
+  const fahrenheit = temp * (9 / 5) - 459.67;
+  let current = 'celcius';
+  let displayTemp = celcius;
+  let displaySym = '°C';
+  const changeTo = {
+    celcius: 'fahrenheit',
+    fahrenheit: 'kelvin',
+    kelvin: 'celcius',
+  };
+  const getCard = () => {
+    const card = document.createElement('div');
+    const weatherInfo = document.createElement('div');
+    const weatherDesc = document.createElement('p');
+    weatherDesc.textContent = desc;
+    const tempText = document.createElement('p');
+    tempText.textContent = `${Math.round(celcius)}°C`;
+    weatherInfo.append(weatherDesc, tempText);
+
+    const cardControls = document.createElement('div');
+    const btnChangeUnit = document.createElement('button');
+    btnChangeUnit.textContent = 'change Unit';
+    btnChangeUnit.onclick = () => {
+      current = changeTo[current];
+      console.log(current);
+
+      if (current === 'celcius') {
+        displayTemp = celcius;
+        displaySym = '°C';
+      }
+      if (current === 'fahrenheit') {
+        displayTemp = fahrenheit;
+        displaySym = '°F';
+      }
+      if (current === 'kelvin') {
+        displayTemp = kelvin;
+        displaySym = 'K';
+      }
+      tempText.textContent = `${Math.round(displayTemp)}${displaySym}`;
+    };
+    cardControls.append(btnChangeUnit);
+    card.append(weatherInfo, cardControls);
+    return card;
+  };
+
+  return { getCard, desc };
+};
+
+let weatherCard = WeatherAtCard();
+
+function displayWeather(json) {
+  console.log(json);
+  const { temp } = json.main;
+  const { description } = json.weather[0];
+  weatherCard = WeatherAtCard(description, temp);
+  main.innerHTML = '';
+  main.append(LocationForm, weatherCard.getCard());
+}
+
+getGeoLocation('london')
+  .then((response) => {
+    const { lat } = response[0];
+    const { lon } = response[0];
+    getWeather(lat, lon).then(displayWeather);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+main.append(LocationForm);
